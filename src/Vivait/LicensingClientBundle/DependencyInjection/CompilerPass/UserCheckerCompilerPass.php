9c5d84f138c04239fff95b3cbb5efa6bc097c84e
Jack Bentley <jack@vivait.co.uk>
2014-12-15 12:05:45 +0000

    Initial Commit

---
+++ src/Vivait/LicensingClientBundle/DependencyInjection/CompilerPass/UserCheckerCompilerPass.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Vivait\LicensingClientBundle\DependencyInjection\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class UserCheckerCompilerPass implements CompilerPassInterface
+{
+
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $container
+     *
+     * @api
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $container->getDefinition("security.user_checker")->addArgument('%vivait.vivadesk.tenant.licensekey%')->setClass('Vivait\LicensingBundle\LicensingUserChecker');
+    }
+}
---
+++ src/Vivait/LicensingClientBundle/LicensingUserChecker.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Vivait\LicensingClientBundle;
+
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\SecurityContext;
+use \GuzzleHttp\Client as Guzzle;
+use Symfony\Component\Serializer\Encoder\JsonDecode;
+
+class LicensingUserChecker implements UserCheckerInterface
+{
+    private $license;
+    private $licenseKey;
+
+    private $guzzle;
+
+    /**
+     * Constructor
+     *
+     * @param $licenseKey
+     * @internal param SecurityContext $securityContext
+     * @internal param Doctrine $doctrine
+     */
+    public function __construct($licenseKey)
+    {
+        $this->licenseKey = $licenseKey;
+        $this->guzzle     = new Guzzle(
+            [
+                'base_url' => 'http://licensing.dev/api/',
+                'defaults' => [
+                    'headers' => [
+                        'api-key' => '9kmMhL5Cz68nwUUZoFWV'
+                    ]
+                ]
+            ]
+        );
+    }
+
+    private function isLicenseValid()
+    {
+        return $this->license['status'] == "active";
+    }
+
+    private function stopLogon($errorMessage = "Inactive license")
+    {
+        throw new AccountExpiredException($errorMessage);
+    }
+
+    /**
+     * Checks the user account before authentication.
+     *
+     * @param UserInterface $user a UserInterface instance
+     */
+    public function checkPreAuth(UserInterface $user)
+    {
+
+    }
+
+    /**
+     * Checks the user account after authentication.
+     *
+     * @param UserInterface $user a UserInterface instance
+     */
+    public function checkPostAuth(UserInterface $user)
+    {
+        try {
+            /** @noinspection PhpVoidFunctionResultUsedInspection */
+            $response = $this->guzzle->get('licenses/' . $this->licenseKey);
+        } catch(\Exception $e) {
+            $this->stopLogon($e->getMessage());
+            return;
+        }
+
+        $body = $response->getBody()->getContents();
+
+        $decoder = new JsonDecode(true);
+        $this->license = $decoder->decode($body, null);
+
+        if(!$this->isLicenseValid())
+            $this->stopLogon();
+    }
+}
